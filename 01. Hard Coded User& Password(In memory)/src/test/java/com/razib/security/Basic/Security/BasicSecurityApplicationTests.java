61d1f2863d0cbd9ae9a76f10216bae531a2bc395
RazibHossain <razib@divine-it.net>
2022-06-07 07:18:05 +0600

    In memory User and Password

---
+++ 01. Hard Coded User& Password(In memory)/src/test/java/com/razib/security/Basic/Security/BasicSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.razib.security.Basic.Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
